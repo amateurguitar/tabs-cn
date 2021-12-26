162d32591d778028ae5585a5a1efd5a98a7d7cf5
YJ Park <yjpark@gmail.com>
2021-12-26 21:18:48 +0800

    adding existing tabs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use notation_tab::prelude::*;
 
 pub mod test;
+pub mod songs;
 
 pub fn main() {
     write_tab(&test::new_tab(), "tabs/test.ron");
+    //songs
+    write_tab(&songs::pu_shu::bai_hua_lin::new_tab(), "tabs/songs/pu_shu/bai_hua_lin.ron");
+    write_tab(&songs::jay::long_juan_feng::new_tab(), "tabs/songs/jay/long_juan_feng.ron");
 }
